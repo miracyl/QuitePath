e4f9ae6550d986af888bd86015cec40ac35eda6c
batrakov-sergey <batrakov-sergey@invix-solution.com>
2026-04-08 19:45:50 +0300

    add:

---
+++ cmd/main/main.go
@@ -0,0 +1,30 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os"
+
+// 	"github.com/joho/godotenv"
+// )
+// func connectToString() string  {
+// 	err:= godotenv.Load()
+
+// 	if err!= nil {
+// 		log.Fatal("Fatal error of load .env")
+// 	}
+// 	dbHost := os.Getenv("POSTGRES_HOST")
+// 	dbPort := os.Getenv("POSTGRES_HOST")
+// 	dbUser := os.Getenv("POSTGRES_USER")
+// 	dbPassword := os.Getenv("POSTGRES_PASSWORD")
+// 	db := os.Getenv("POSTGRES_DB")
+
+// 	connectionString := fmt.Sprintf(
+// 		"host=$s port=$s user=$s password=$s dbname=$s",dbHost,dbPort,dbUser,dbPassword,db,
+// 	)
+// 	return connectionString
+// }
+func main() {
+
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module QuitePath
+
+go 1.25.1
